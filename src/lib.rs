5a570e3f6dae027a7e8cb0aa7133a897f00fea03
Tom Schuster <evilpies@gmail.com>
2018-08-02 15:57:51 +0200

    Clean up macOS code, fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate libc;
 
 use std::path::{Path, PathBuf};
-use std::io::{self, Result};
+use std::io;
 use std::fs::File;
 
 #[cfg(target_os="linux")]
@@ -12,8 +12,11 @@ use std::os::unix::io::AsRawFd;
 
 #[cfg(not(target_os="linux"))]
 use std::ffi::OsString;
-#[cfg(not(target_os="linux"))]
+#[cfg(target_os="macos")]
 use std::os::unix::ffi::OsStringExt;
+
+#[cfg(target_os="macos")]
+const F_GETPATH : i32 = 50;
 
 /// An extension trait for `std::fs::File` providing an `path` method.
 ///
@@ -21,34 +24,34 @@ use std::os::unix::ffi::OsStringExt;
 pub trait FilePath {
     // Returns the file path this file points to.
     // Not every file has a path and the path can change after opening.
-    fn path(&self) -> Result<PathBuf>;
+    fn path(&self) -> io::Result<PathBuf>;
 }
 
 impl FilePath for File {
     #[cfg(target_os="linux")]
-    fn path(&self) -> Result<PathBuf> {
+    fn path(&self) -> io::Result<PathBuf> {
         let fd = self.as_raw_fd();
         let path = Path::new("/proc/self/fd/").join(fd.to_string());
         read_link(path)
     }
 
     #[cfg(target_os="macos")]
-    fn path(&self) -> Result<PathBuf> {
+    fn path(&self) -> io::Result<PathBuf> {
         let fd = self.as_raw_fd();
         let mut path = vec![0; libc::PATH_MAX as usize + 1];
 
         unsafe {
-            let len = libc::fcntl(fd, 50, path.as_mut_ptr());
-            if len < 0 {
+            if libc::fcntl(fd, F_GETPATH, path.as_mut_ptr()) < 0 {
                 return Err(io::Error::last_os_error());
             }
+
             path.retain(|&c| c != 0);
             Ok(PathBuf::from(OsString::from_vec(path)))
         }
     }
 
     #[cfg(windows)]
-    fn path(&self) -> Result<PathBuf> {
+    fn path(&self) -> io::Result<PathBuf> {
         let mut path = vec![0; MAX_PATH + 1];
         let len = fileapi::GetFinalPathNameByHandleW(self.as_raw_handle(),
                                                      path.as_mut_ptr(),
