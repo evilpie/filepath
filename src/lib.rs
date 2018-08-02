6b9b774aefe33272e92c87e296c0aafe0b27470c
Tom Schuster <evilpies@gmail.com>
2018-08-02 15:24:43 +0200

    implement macos

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+extern crate libc;
+
 use std::path::{Path, PathBuf};
-use std::io::Result;
+use std::io::{self, Result};
 use std::fs::File;
 
 #[cfg(target_os="linux")]
 use std::fs::read_link;
 #[cfg(target_os="linux")]
 use std::os::unix::io::AsRawFd;
+
+#[cfg(not(target_os="linux"))]
+use std::ffi::OsString;
+#[cfg(not(target_os="linux"))]
+use std::os::unix::ffi::OsStringExt;
 
 /// An extension trait for `std::fs::File` providing an `path` method.
 ///
@@ -22,6 +29,22 @@ impl FilePath for File {
         let fd = self.as_raw_fd();
         let path = Path::new("/proc/self/fd/").join(fd.to_string());
         read_link(path)
+    }
+
+    #[cfg(target_os="macos")]
+    fn path(&self) -> Result<PathBuf> {
+        let fd = self.as_raw_fd();
+        let mut path = vec![0; libc::PATH_MAX as usize + 1];
+
+        unsafe {
+            let res = libc::fcntl(fd, 50, path.as_mut_ptr());
+            if res < 0 {
+                return Err(io::Error::last_os_error());
+            }
+        }
+
+        println!("{:?}", path);
+        Ok(PathBuf::from(OsString::from_vec(path)))
     }
 
     #[cfg(windows)]
@@ -41,12 +64,13 @@ impl FilePath for File {
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
     use std::fs::File;
     use FilePath;
 
     #[test]
     fn simple() {
         let file = File::open("/tmp/foobar").unwrap();
-        println!("{:?}", file.path());
+        assert_eq!(file.path().unwrap(), PathBuf::from(r"/tmp/foobar"));
     }
 }
